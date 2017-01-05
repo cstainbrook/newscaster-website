92efe5e958294023badbce29299a3c3f578f44dc
Cooper Stainbrook <Pepsihuck@Cooper-Stainbrooks-MacBook-Pro.local>
2017-01-05 16:30:04 -0700

    chahanging debug

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=7029, debug=True, threaded=True)
+    app.run(host='0.0.0.0', port=7029, debug=False, threaded=True)
