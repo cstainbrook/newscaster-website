c2394070a2c56d98740f96c11767fadbd6f30660
Cooper Stainbrook <cstainbrook@Coopers-MacBook-Pro.local>
2017-12-16 15:33:15 -0700

    updating reel december 2017

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=7044, debug=False, threaded=True)
+    app.run(host='0.0.0.0', port=7045, debug=False, threaded=True)
